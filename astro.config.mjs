60e4063902fef647ad0ddae4154cdf34820bcc58
Sutari Chaitra <83217099+chaitrasutari@users.noreply.github.com>
2025-08-04 22:40:08 -0500

    fixed base urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-  output: 'static',
-  base: 'chaitrasutari.github.io'
+  output: 'static'
 });
